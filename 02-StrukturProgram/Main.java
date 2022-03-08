4aeabce625c6ec4de6371182ec4273aafaa926c1
ifandika <ifandika014@gmail.com>
2022-03-08 19:32:34 +0700

    Tambah Materi Baru

---
+++ 02-StrukturProgram/Main.java
@@ -0,0 +1,14 @@
+public class Main { // class Main
+
+    // public = class ini terbuka untuk umum, bisa di akses oleh siapa saja
+    // class = class untuk program
+    // Main = nama class
+    
+    public static void main(String[] args) { // ini adalah method, gerbang utama program
+        
+        // public = method public ini akan di akses oleh system sehingga harus public
+        // static = 
+        // void = 
+        // main = nama default dari java untuk menentukan ini adalah main program, program utama
+    }
+}
